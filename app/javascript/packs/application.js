105b5ccaec8f7209856b76774a7efbb7a2338461
Wanying Kwok <kwyfarrah@gmail.com>
2020-10-16 01:31:52 +0900

    favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -24,11 +25,12 @@ require("channels")
 
 // External imports
 import "bootstrap";
+// import { scroll } from '../components/pagescroll.js';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  // scroll();
 });
