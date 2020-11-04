77a24a08fc06fd4e77aed150dfdba001bfcd576d
Wanying Kwok <kwyfarrah@gmail.com>
2020-11-04 23:13:18 +0900

    skill css

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "skills", to: "skills#index"
+  get "works", to: "works#index"
+  get "galleries", to: "galleries#index"
 end
